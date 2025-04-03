c80af1938f8404b54570cac0d9b5ef2a3539766e
hind hisham <hind.hisham.alsadig@gmail.com>
2025-04-03 10:00:10 +0300

    new axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE === "development" ? "http://localhost:3500/api/v1" : "https://wehda.onrender.com/api/v1",
+    baseURL:import.meta.env.MODE === "development" ? "http://localhost:3500/api/v1" : "/api/v1",
     withCredentials: true
 });
