dd8987aab89914fd795164839bdd2fc80f2ae4e2
hind hisham <hind.hisham.alsadig@gmail.com>
2025-09-30 00:04:44 +0300

    db fix

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-    } catch (error) {        
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
