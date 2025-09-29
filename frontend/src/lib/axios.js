e7d933909e0d9deb8304be9d13c2223b3a734f61
hind hisham <hind.hisham.alsadig@gmail.com>
2025-09-30 00:58:59 +0300

    url add

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:"https://wehda.io/api/v1",
   withCredentials: true
 });
