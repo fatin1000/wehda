ef4ce78f369aad09c5320d86ab6de809fd091ac1
hind hisham <hind.hisham.alsadig@gmail.com>
2025-09-30 00:54:14 +0300

    login fix

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ const __dirname = path.resolve();
 
 app.use(cors({
     origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
   }));
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser())
