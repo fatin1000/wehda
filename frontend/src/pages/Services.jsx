8a73ec5a0d5e66b1825a72aab1e74218e8b38980
hind hisham <hind.hisham.alsadig@gmail.com>
2025-10-04 09:01:59 +0300

    trans fix2

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -88,7 +88,7 @@ const Services = () => {
                     {(isLoading) ? (<div>{t("loading")}</div>) : (isError) ? (<div>{t("services.errorLoadingUsers")}</div>) :
                         (DBservices[0] === "start") ? null :
                             (isSuccess && DBservices?.length === 0) ?
-                                (<div className="flex justify-center">{t("services.noProvidersFound")} {t(`fields.${service}`)}  😓</div>)
+                                (<div className="flex justify-center">{t("services.noProvidersFound")} {t(`fields.${service}`)} {t("services.in")} {t(`cities.${city}`)}  😓</div>)
                                 : (
                                     <div>
                                         <h3 className="text-2xl font-semibold mb-1">{t("services.serviceProviders")}</h3>
